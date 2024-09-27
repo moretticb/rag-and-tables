e56e2ff7046d21af42fff134bae9b247739cc3d5
Caio Benatti Moretti <caiodba@gmail.com>
2024-09-27 10:34:05 +0200

    organizing files and adding more details for reproducibility

--- figs/reading_pdf/read_pdftotext.py
+++ figs/reading_pdf/read_pdftotext.py
@@ -1,6 +1,6 @@
 import pdftotext # v2.2.2
 
-with open("tables.pdf", "rb") as f:
+with open("../../pdf/tables.pdf", "rb") as f:
     pages = list(pdftotext.PDF(f))
 
 print(pages[0])
--- figs/reading_pdf/read_pypdf.py
+++ figs/reading_pdf/read_pypdf.py
@@ -1,6 +1,6 @@
 from pypdf import PdfReader # v4.3.1
 
-reader = PdfReader("tables.pdf")
+reader = PdfReader("../../pdf/tables.pdf")
 
-print(reader.pages[1].extract_text())
+print(reader.pages[0].extract_text())
 
