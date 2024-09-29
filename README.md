50b51c6c2185fbf567b08b35fb893fef9d03bab5
Caio Benatti Moretti <caiodba@gmail.com>
2024-09-29 03:29:55 +0200

    minor fixes

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Reproducible materials from the [RAG decomposition blog post](https://www.xebia.com/blog) on how to break down tables into better chunk formats, improving retrieval.
 
+## Setup
+
+Make sure to have poetry installed and install the project dependencies:
+
+```bash
+poetry install
+```
 
 ## Running the experiment
 
@@ -9,7 +16,7 @@ Run `run_experiment.sh`:
 
 ```bash
 cd experiment
-./run_experiment.sh
+poetry run ./run_experiment.sh
 ```
 
 It will generate all chunks from the three approaches (decomposition, raw parsing with PyPDF and reverse engineering), calculate the embeddings and generate the final plot for comparisons.
--- experiment/run_experiment.sh
+++ experiment/run_experiment.sh
@@ -5,9 +5,9 @@ cd .. # main project directory
 echo "========== GENERATING CHUNKS ==========\n"
 
 cd chunks
-[ -e chunk_decomp.txt ] && rm chunk_decomp.txt && echo "Removing current chunk_decomp.txt"
-[ -e chunk_pypdf.txt ]  && rm chunk_pypdf.txt  && echo "Removing current chunk_pypdf.txt"
-[ -e chunk_reveng.txt ] && rm chunk_reveng.txt && echo "Removing current chunk_reveng.txt"
+#[ -e chunk_decomp.txt ] && rm chunk_decomp.txt && echo "Removing current chunk_decomp.txt"
+#[ -e chunk_pypdf.txt ]  && rm chunk_pypdf.txt  && echo "Removing current chunk_pypdf.txt"
+#[ -e chunk_reveng.txt ] && rm chunk_reveng.txt && echo "Removing current chunk_reveng.txt"
 poetry run ./generate_all_chunks.sh
 cd ..
 
